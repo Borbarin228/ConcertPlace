3631a67784c14572841a0a6ff4ed671428eb8c9c
Borbarin228 <114693651+Borbarin228@users.noreply.github.com>
2025-04-01 10:25:07 +0500

    миграции

---
+++ database/migrations/2025_03_17_092159_create_concerts_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('concerts', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->string('city');
+            $table->string('place');
+            $table->foreignId('category_id')->index();
+            $table->date('start_at');
+            $table->boolean('is_accepted');
+        });
+
+        Schema::create('ticket_categories', function (Blueprint $table){
+            $table->id();
+            $table->timestamps();
+            $table->string('name');
+            $table->string('description');
+            $table->foreignId('owner_id')->index();
+            $table->double('price');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('concerts');
+        Schema::dropIfExists('ticket_categories');
+    }
+};
